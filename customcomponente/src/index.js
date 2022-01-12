f15ea56e95b3faa802a9fa592b63f002ea52682c
Your Name <you@example.com>
2022-01-12 00:02:26 -0300

    customizaçao de componente

---
+++ customcomponente/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import CustomComponente1 from './CustomComponente1'
+import CustomComponente2 from './CustomComponente2'
+
+
+
+//ReactDOM.render(<App />,document.getElementById('root'));
+//ReactDOM.render(<CustomComponente1 />,document.getElementById('root'));
+ReactDOM.render(<CustomComponente2 />,document.getElementById('root'));
--- jogo/src/index.js
+++ jogo/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import JogoDaVelha from './JogoDaVelha';
 
+
 ReactDOM.render(<JogoDaVelha />,document.getElementById('root'));
