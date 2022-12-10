3087ddd45611c6c24cb3fd337ee13bef261dfc32
Your Name <you@example.com>
2022-12-10 15:45:00 -0300

    add

---
+++ jsx/anotacao.js
@@ -0,0 +1,18 @@
+// JSX
+
+// Introduzindo JSX
+
+// Considere esta declaração de variável:
+
+// const element = <h1>Hello, world!</h1>;
+
+// Esta sintaxe estranha de tags não é uma string, nem HTML.
+
+
+// Por que JSX?
+// O React adota o fato de que a lógica de renderização é inerentemente acoplada com outras lógicas de UI: como eventos são manipulados, como o state muda com o tempo e como os dados são preparados para exibição.
+
+// Ao invés de separar tecnologias artificialmente colocando markup e lógica em arquivos separados, o React separa conceitos com unidades pouco acopladas chamadas “componentes” que contém ambos
+
+// O React não requer o uso do JSX.
+
