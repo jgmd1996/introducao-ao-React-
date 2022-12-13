e061b7aea8c2fb63d695d650361f5e4f221d3aff
joao gabriel <jgmd1996@gmail>
2022-12-13 20:58:21 -0300

    Hook useefect

--- introducao/src/App.js
+++ introducao/src/App.js
@@ -5,14 +5,15 @@ import Pessoa from './props/Pessoa';
 import List from './props/List';
 import Evento from './eventos/Evento';
 import Form from './eventos/Form';
+import Usestate from './eventos/Usestate';
 
 function App() {
 
   
   return (
     <div>
-      <Evento numero={2}/>
-      <Form />
+      {/* <Evento numero={2}/> */}
+     <Usestate />
     </div>
   );
 }
